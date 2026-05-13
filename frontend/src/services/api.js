574e0f2e7d27b0a87ef28e47224c34342d31b88d
Venkata Reddy <bommavaramvenkat2003@gmail.com>
2026-05-13 22:49:03 +0530

    feat: make frontend api configuration deployment ready

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,21 +1,26 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL:
+    import.meta.env
+      .VITE_API_URL,
+
   headers: {
-    "Content-Type": "application/json",
+    "Content-Type":
+      "application/json",
   },
 });
 
-export const valuateProperty = async (
-  propertyData
-) => {
-  const response = await API.post(
-    "/valuate",
-    propertyData
-  );
+export const valuateProperty =
+  async (propertyData) => {
 
-  return response.data;
-};
+    const response =
+      await API.post(
+        "/valuate",
+        propertyData
+      );
+
+    return response.data;
+  };
 
 export default API;
