b267c0e7c9283d8107ccbf915d770861688d84d2
Venkata Reddy <bommavaramvenkat2003@gmail.com>
2026-05-13 22:27:17 +0530

    feat: integrate property form into valuation page

---
+++ frontend/src/pages/ValuatePage.jsx
@@ -0,0 +1,11 @@
+import PropertyForm from "../components/PropertyForm";
+
+function ValuatePage() {
+  return (
+    <div className="min-h-screen bg-black px-6 py-20">
+      <PropertyForm />
+    </div>
+  );
+}
+
+export default ValuatePage;
